721eca6b06423a7146d660693db5c0e9d507d635
Aitor González Fernández <reimashi@gmail.com>
2014-11-14 10:39:19 +0100

    Renderizado del home

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,4 +1,13 @@
+var swig    = require('swig');
 
 // Genera la pagina de inicio
 exports.index = function(req, res) {
+    var baseweb = swig.compileFile('views/base.html');
+    var formreg = swig.compileFile('views/form-registry.html');
+
+    res.status(200);
+
+    res.send(baseweb({content: [formreg()]}));
+
+    res.end();
 }
