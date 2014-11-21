6b42d0e8e7a5ffe3935d3e80274650fb15d0e946
Aitor González Fernández <reimashi@gmail.com>
2014-11-21 10:33:07 +0100

    Añadiendo métodos para los comentarios y los likes

--- models/post-model.js
+++ models/post-model.js
@@ -1,9 +1,59 @@
 var mongoose    = require('mongoose');
 var Schema      = mongoose.Schema;
 
-var userSchema = new Schema ({
-  _id:        { type: Schema.Types.ObjectId },
+var postSchema = new Schema ({
   title:      { type: String, required: true },
   body:       { type: String, required: true },
-  likes:      { type: Number, min: 0, required: true }
+  author:     { type: Schema.Types.ObjectId, ref: 'User' },
+  likes:      [{
+      user:   { type: Schema.Types.ObjectId, ref: 'User' }
+  }],
+  comments    [{
+      body:         { type: String, required: true },
+      user:         { type: Schema.Types.ObjectId, ref: 'User' },
+      created:      { type : Date, default : Date.now }
+  }]
 });
+
+postSchema.methods = {
+    addComment: function (user, comment, cb) {
+        this.comments.push({
+          body: comment,
+          user: user._id
+        });
+
+        this.save(cb);
+    },
+
+    addLike: function (user, cb) {
+        var index = utils.indexof(this.likes, { user: user._id });
+
+        if (~index) return cb('Este usuario ya le habia dado a like.');
+        else {
+            this.likes.push({
+                user: user._id;
+            });
+            this.save(cb);
+        }
+    },
+
+    deleteLike:function (user, cb) {
+        var index = utils.indexof(this.likes, { user: user._id });
+
+        if (~index) this.comments.splice(index, 1);
+        else return cb('Este usuario no le habia dado a like.');
+        this.save(cb);
+    }
+}
+
+postSchema.statics = {
+    list: function (limit, step, cb) {
+        this.find({})
+            .limit(limit)
+            .sort({'createdAt': -1})
+            .skip(limit * step)
+            .exec(cb);
+    }
+}
+
+mongoose.model('Post', postSchema);
