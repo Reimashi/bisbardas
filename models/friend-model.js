5b78133db05e4c1dc074bc40429d3e12f5ca14e7
Aitor González Fernández <reimashi@gmail.com>
2014-11-21 10:41:24 +0100

    Añadidos métodos para aceptar o ignorar amigos

--- models/friend-model.js
+++ models/friend-model.js
@@ -2,8 +2,27 @@ var mongoose    = require('mongoose');
 var Schema      = mongoose.Schema;
 
 var friendSchema = new Schema ({
-  user:       { type: Schema.Types.ObjectId, required: true },
-  friend:     { type: Schema.Types.ObjectId, required: true },
+  user:       { type: Schema.Types.ObjectId, required: true, ref = 'User' },
+  friend:     { type: Schema.Types.ObjectId, required: true, ref = 'User' },
   acepted:    { type: Boolean, default: false, required: true },
   ignored:    { type: Boolean, default: false, required: true }
 });
+
+postSchema.methods = {
+    accept: function (cb) {
+        this.acepted = true;
+        this.save(cb);
+    },
+    ignore: function (cb) {
+        this.ignored = true;
+        this.save(cb);
+    }
+}
+
+postSchema.statics = {
+    list: function (user, cb) {
+        this.find({ user: user._id }).exec(cb);
+    }
+}
+
+mongoose.model('Friend', friendSchema);
