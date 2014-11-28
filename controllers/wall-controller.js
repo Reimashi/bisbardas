91b1589f1685d80c51b031e63f72a7e7bcbdf683
TheHollowEsp <hrlopez@esei.uvigo.es>
2014-11-28 11:31:15 +0100

    Añadido código de prueba en wall.

--- controllers/wall-controller.js
+++ controllers/wall-controller.js
@@ -1,6 +1,7 @@
 var swig 			= require('swig');
+//var util 			= require('util');
 var mongoose 		= require ('mongoose');
-var validator 		= require('express-validator');
+//var validator 		= require('express-validator');
 var postModel 		= require('../models/post-model')();
 
 
@@ -15,7 +16,6 @@ exports.index = function(req, res) {
 			var postsRenders = Array();
 			//Mete el form de post nuevo
 			postsRenders.push(piecenewpost());
-
 			//Renderiza los posts
 			posts.forEach(function(post) {
 				//Revisar
@@ -32,8 +32,8 @@ exports.index = function(req, res) {
 
 // TODO: Añade un post a la base de datos
 exports.addPost = function(req, res) {
-  //Comprobaciones (no vacio)
-	req.checkBody('title', 'El titulo no puede estar vacio').notEmpty();
+  /*Comprobaciones (no vacio)
+  req.checkBody('title', 'El titulo no puede estar vacio').notEmpty();
   req.checkBody('body', 'El post debe tener contenido').notEmpty();
   var errors = req.validationErrors();
   console.log(errors);
@@ -44,8 +44,11 @@ exports.addPost = function(req, res) {
   else {
   	//Recuperar post e insertar
 		var npost = new postModel(req.body);
+		npost.save(function (err) {
+			if (err) return handleError(err);
+		});
 		res.send("Post publicado correctamente");
-  }
+  }*/
 }
 
 // TODO: Obtiene un post en concreto
@@ -53,22 +56,41 @@ exports.getPost = function(req, res) {
 	var piecepost = swig.compileFile('views/piece-post.html');
 	var id = req.body.id;
 
-	post.get(function (err, post){
-		if (err) res.json(err);
-		else {
-			//
-			res.send("elpost");
+	postModel
+	.findOne()
+	.where('post._id').equals(id)
+	.exec(
+			function (err, post) {
+				if (err) return handleError(err);
+				else {
+					console.log('Tengo tu post');
+					res.send(post);
+				}
 			}
-	});
+		);
+
+
 }
 
 // TODO: Borra el post pasado como parametro
 exports.deletePost = function(req, res) {
-	//Obtener post de algun modo y...
+	var id = req.body.id;
 	post.delete(function (err, post){
 		if (err) res.json(err);
-		else {}
-});
+		else{
+		postModel
+		.findOne()
+		.where('post._id').equals(id)
+		.exec(
+				function (err, post) {
+					if (err) return handleError(err);
+					else {
+						console.log('Vamos a eliminar');
+					}
+				}
+			);
+		}
+	});
 }
 
 // Faltan los likes
