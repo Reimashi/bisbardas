96ba8083d7e09f6fc40363d061f8da9a782c9a85
Aitor González Fernández <reimashi@gmail.com>
2014-11-28 10:54:44 +0100

    Añadida librería de validación y para servir favicon

--- config/swig.js
+++ config/swig.js
@@ -1,14 +1,18 @@
+var favicon     = require('serve-favicon');
 var config      = require('./server');
 var path        = require('path');
 var swig        = require('swig');
 var bodyParser  = require('body-parser');
+var validator   = require('express-validator');
 
 module.exports = function (app) {
     app.engine('html', swig.renderFile);
     app.set('view engine', 'html');
     app.set('views', path.join(__dirname, 'views'));
     app.set('view cache', !config.debug);
+    app.use(favicon('./public/img/favicon.ico'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(validator());
     swig.setDefaults({ cache: config.debug ? false : 'memory' });
 }
