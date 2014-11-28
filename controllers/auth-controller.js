d6f28ab2989ab266732d90f08464fb2dbd6b3dee
Aitor González Fernández <reimashi@gmail.com>
2014-11-28 10:55:17 +0100

    Añadido formulario de login avanzado

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,6 +1,27 @@
+var swig 			= require('swig');
 
-// TODO: Genera la pagina de inicio de sesión, y recibe los datos del POST
+// Genera la pagina de inicio de sesión
+exports.index = function(req, res) {
+    var baseweb = swig.compileFile('views/base.html');
+    var loginform = swig.compileFile('views/form-login.html');
+
+    var viewparams = {
+        'content': [loginform({ "formLoginError": req.param('form-error', false) })]
+    }
+
+    res.status(200).send(baseweb(viewparams)).end();
+}
+
+// TODO: Realiza el inicio de sesión
 exports.login = function(req, res) {
+    req.assert('username', 'Email required').notEmpty();
+    req.assert('username', 'Malformed email').isEmail();
+    req.assert('password', 'Password required').notEmpty().isEmail();
+
+    var errors = req.validationErrors();
+    if (errors) {
+
+    }
 }
 
 // TODO: Cierra sesión y redirecciona a home
