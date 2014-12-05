72a53e1912c7767d26c1a9e6d7c5905c8bcc4a8e
Adrian Gonzalez Barbosa <adrian.gonzalez.barbosa@gmail.com>
2014-12-05 10:16:35 +0100

    Improvements

--- controllers/friend-controller.js
+++ controllers/friend-controller.js
@@ -1,6 +1,24 @@
+var swig 			= require('swig');
+var mongoose 		= require ('mongoose');
+var friendModel 		= require('../models/friend-model')();
 
 // TODO: Genera la pagina del muro (Si usuario logueado)
 exports.index = function(req, res) {
+  var baseweb = swig.compileFile('views/base.html');
+  var piecefriend = swig.compileFile('views/piece-friend.html');
+
+  friendModel.list(20, 0, function (err, posts) {
+    var friendsRenders = Array();
+    //Renderiza los posts
+    friend.forEach(function(post) {
+      //Revisar
+      friendsRenders.push(piecefriend(post));
+    });
+    res.status(200);
+    //Manda todo
+    res.send(baseweb({content: postsRenders}));
+    res.end();
+  });
 }
 
 // TODO: Añade una peticion de amistad
