3f9c2de741d15542f41ee7e73fbac993b7d8cb0c
TheHollowEsp <hrlopez@esei.uvigo.es>
2014-12-06 22:07:53 +0100

    Mejoras en /wall

--- controllers/wall-controller.js
+++ controllers/wall-controller.js
@@ -4,39 +4,43 @@ var postModel 		= require('../models/post-model')();
 
 
 
-// TODO: Genera la pagina del muro (Si usuario logueado)
+//FIXME: Los "...leer completa" no son clickeables (no se puede mandar al get con ellos, por ende)
 exports.index = function(req, res) {
 	var baseweb = swig.compileFile('views/base.html');
+	var buttonnew = swig.compileFile('views/button-newpost.html');
 	var piecepost = swig.compileFile('views/piece-post.html');
-	var piecenewpost = swig.compileFile('views/piece-newpost.html');
+
 
 	postModel.list(20, 0, function (err, posts) {
 		var postsRenders = Array();
-		//Mete el form de post nuevo
-		postsRenders.push(piecenewpost());
-		//Renderiza los posts
+		postsRenders.push(buttonnew());
 		posts.forEach(function(post) {
-			//Revisar
 			postsRenders.push(piecepost(post));
 		});
 		res.status(200);
 		//Manda todo
 		res.send(baseweb({content: postsRenders}));
+
+
 		res.end();
 	});
 }
 
-// TODO: Añade un post a la base de datos
+/*
+ *	Todos los /post tienen error en el CSS. Cannot GET /post/static/css/common.css
+ */
+
+
+
+// FIXME: Hacer un redirect tras meter el post, formatear mensaje bien
 exports.addPost = function(req, res) {
 	var baseweb = swig.compileFile('views/base.html');
-	//Recuperar post e insertar
 	var npost = new postModel(req.body);
 	npost.save(function (err) {
-		if (err) return handleError(err);
+		if (err) return new Error(err);
 		else {
 			res.status(200);
-			res.send("Post publicado correctamente");
-			//FIXME: Hacer un redirect
+			res.redirect(200, '../wall');
 			res.end();
 		}
 	});
@@ -44,13 +48,19 @@ exports.addPost = function(req, res) {
 
 }
 
-// En este muestra el formulario y en el anterior maneja la respuesta
+// FIXME: Por alguna razon hay un "object Object" por ahi metido
 exports.addPostForm = function(req, res) {
-
+	var baseweb = swig.compileFile('views/base.html');
+	var piecenewpost = swig.compileFile('views/piece-newpost.html');
+	piecenewpost();
+	res.status(200);
+	res.send(baseweb({content: piecenewpost}));
+	res.end();
 }
 
 // TODO: Obtiene un post en concreto
 exports.getPost = function(req, res) {
+	var baseweb = swig.compileFile('views/base.html');
 	var piecepost = swig.compileFile('views/piece-post.html');
 	var id = req.body.id;
 
@@ -63,7 +73,7 @@ exports.getPost = function(req, res) {
 			else {
 				console.log('Tengo tu post');
 				res.status(200);
-				res.send(piecepost(post));
+				res.send(baseweb({content: piecepost(post)}));
 				res.end();
 			}
 		}
