c228cd752e47ae171f5e93115fa38dc578c1401c
Adrián González Barbosa <adrian.gonzalez.barbosa@gmail.com>
2014-12-07 11:39:14 +0100

    Adapted view

--- controllers/friend-controller.js
+++ controllers/friend-controller.js
@@ -1,6 +1,7 @@
-var swig 			= require('swig');
-var mongoose 		= require ('mongoose');
-var friendModel 		= require('../models/friend-model')();
+var swig 		     	= require('swig');
+var mongoose 		  = require ('mongoose');
+var friendModel 	= require('../models/friend-model')();
+var userModel     =require('../models/user-model')();
 
 // TODO: Genera la pagina del muro (Si usuario logueado)
 exports.index = function(req, res) {
@@ -9,24 +10,27 @@ exports.index = function(req, res) {
 
   var viewparams = {
     'content': [piecefriend()]
-  }
+  };
+  console.log("Prueba");
+  console.log(req);
+  //console.log(friendModel.list(userModel,1));
   res.status(200);
   res.send(baseweb(viewparams));
   res.end();
-}
+};
 
 // TODO: Añade una peticion de amistad
 exports.add = function(req, res) {
-}
+};
 
 // TODO: Acepta una peticion de amistad
 exports.accept = function(req, res) {
-}
+};
 
 // TODO: Borra una peticion de amistad
 exports.delete = function(req, res) {
-}
+};
 
 // TODO: Ignora un amigo por su id de usuario
 exports.ignore = function(req, res) {
-}
+};
