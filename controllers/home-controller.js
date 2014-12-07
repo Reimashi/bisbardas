84c67df15ba4534481e0b0db0a583c9d7b43a2e7
Aitor González Fernández <reimashi@gmail.com>
2014-12-07 10:50:29 +0100

    Reparadas rutas relativas

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,13 +1,7 @@
-var swig    = require('swig');
+var swig            = require('swig');
+var userController  = require('./user-controller');
 
 // Genera la pagina de inicio
 exports.index = function(req, res) {
-    var baseweb = swig.compileFile('views/base.html');
-    var formreg = swig.compileFile('views/form-registry.html');
-
-    res.status(200);
-
-    res.send(baseweb({content: [formreg()]}));
-
-    res.end();
+    userController.add(req, res);
 }
